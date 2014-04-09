ad8ce997bd0d34a0eb45148137210163fe602fac
Jakob Leben <jakob.leben@gmail.com>
2014-04-09 13:42:30 -0700

    add script for batch evaluation

---
+++ run/evaluate.sh
@@ -0,0 +1,33 @@
+evaluator=$1
+ref_dir=$2
+eval_dir=$3
+map_file=$4
+
+output=evaluation
+
+echo "" > evaluation
+
+if [ -z $ref_dir ] || [ -z $eval_dir ] || [ -z $map_file ] || [ -z $evaluator ]; then
+  echo "Required arguments: <evaluator program> <samples dir> <evaluated dir> <map file>"
+  exit 1
+fi
+
+process_root() {
+  for reference in `find $ref_dir -name *.mid`
+  do process_reference $reference
+  done
+}
+
+process_reference() {
+  reference=$1
+  base=`basename $reference .mid`
+  object=$eval_dir/$base.onsets
+  cmd="$evaluator -m $map_file -o 50 $reference $object"
+  echo $cmd
+  echo "" >> $output
+  echo "$reference <> $object" >> $output
+  echo ""  >> $output
+  $cmd >> $output
+}
+
+process_root
